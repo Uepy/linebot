3e75ca23429fb3e00af1977a7c7b465f02b9727e
uepy <bdapt.benzen78@gmail.com>
2017-07-25 14:30:15 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+//アクセストークンでCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
+// CurlHTTPClient とシークレットを使いLineBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient,['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+
+// LINE Messaging API がリクエストに付与した署名を取得
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+try {
+  // 署名が正当かチェック 正当であればリクエストをパースして配列へ
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
+}
+//配列に格納された各イベントをループ処理
+foreach ($events as $event) {
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+    error_log('Non message event has come');
+    continue;
+  }
+  if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+    error_log('Non text message has come');
+    continue;
+  }
+  // テキスト返信
+  $bot->replyText($event->getReplyToken(), $event->getText()."ですわよｗ");
+  error_log('Bot has replyed massage. This bot is running on github');
+}
+
+ ?>
