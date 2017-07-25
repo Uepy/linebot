7261f1f51428d06c137106cd1033b50b973063bb
uepy <bdapt.benzen78@gmail.com>
2017-07-25 17:10:47 +0000

    v2

--- pushDeadLine.php
+++ pushDeadLine.php
@@ -9,10 +9,12 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS
 $bot = new \LINE\LINEBot($httpClient,['channelSecret' => getenv('CHANNEL_SECRET')]);
 
 $userID = 'U9a6675ed0946c116097b44bd69024fd4';
-$message = '本日はシフト提出期限です';
+$message = "本日はシフト提出期限です\nシフトは提出しましたか？";
 
-$response = $bot->pushMessage($userID, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message));
 
+if(date("d") == 7 || date("d") ==25){
+    $response = $bot->pushMessage($userID, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message));
+}
 if (!$response->isSucceeded()){
     error_log('failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
 }
