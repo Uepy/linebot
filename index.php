0374c2a7f8f06b11b30aa9d0a3658c1bbc582b4c
uepy <bdapt.benzen78@gmail.com>
2017-07-27 18:17:15 +0000

    v4

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set('Asia/Tokyo');
+
+// lineIDと名前を一致させるためのテーブル名を定義
+define('TABLE_TO_IDENTIFY','tbl_user_identify');
+
 //アクセストークンでCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
@@ -46,6 +50,15 @@ foreach ($events as $event) {
   error_log("\nuserID : " . $event->getUserId());
   
   
+  // ユーザーIDが登録されてなければ登録する
+  if(is_registeredUserId($event->getUserId())){
+    echo "\n Your userID is already registerd.";
+  }else{
+    registerUserId($event->getUserId());
+    echo "\n Your userID is registerd in database now.";
+  }
+  
+  
   // ユーザーからのテキストによってシフト画像を送信する
   // phpの例外処理がよくわからん！から、このtry-catch文はよくないかもしれない
   try{
@@ -86,6 +99,8 @@ foreach ($events as $event) {
       $bot->replyText($event->getReplyToken(),"おはようございます");
     }else if(date('G') >= 12 && date('G') < 18){
       $bot->replyText($event->getReplyToken(),"こんにちは");
+    }else if(date('G') >= 2 && date('G') < 6){
+      $bot->replyText($event->getReplyToken(),"はよ寝なさい");
     }else{
       $bot->replyText($event->getReplyToken(),"こんばんは");
     }
@@ -106,4 +121,71 @@ function replyImageMessage($bot,$replyToken,$originalImageUrl,$previewImageUrl){
     error_log('failed!' . $response->getHTTPStatus . ' ' . $response->getRawBody());
  }
 }
+
+// データベースへの接続を管理するクラス
+class dbConnection{
+  // インスタンス
+  protected static $db;
+  // コンストラクタ
+  private function __construct(){
+    
+    try{
+      // 環境変数からデータベースへの接続情報を取得
+      $url = parse_url(getenv('DATABASE_URL'));
+      // データソース
+      $dsn = sprintf('pgsql:host=%s;dbname=%s',$url('host'),substr($url['path'],1));
+      // 接続を確立
+      self::$db = new PDO($dsn,$url['user'],$url['pass']);
+      // エラー時には例外を投げるように設定
+      self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e){
+      echo 'Connection Error: ' . $e->getMessage();
+    }
+  }
+
+    // シングルトン 存在しない場合のみインスタンス化
+    public static function getConnection(){
+      if(!self::$db){
+        new dbConnection();
+      }
+      return self::$db;
+    }
+    
+}
+
+// TABLE_TO_IDENTIFYにユーザーidを登録
+// is_registeredUserId()と併用すべし
+// ** 狭域かつ限定的にしかシフト通知君を公開していないのでこれでもいいかもしれないが
+// システムが大きくなればこれはよくない気がする **
+function registerUserId($userId){
+  $dbh = dbConnection::getConnection();
+  // pgp_sym_encryptは暗号化 引数は(暗号化するデータ, 共有鍵) 共有鍵はherokuに登録してある
+  $sql = 'insert into ' . TABLE_TO_IDENTIFY . '(userid) values
+  (pgp_sym_encrypt(?,\'' . getenv('DB_ENCRYPT_PASS') . '\') )' ;
+  $sth = $dbh->prepare($sql);
+  $sth->execute(array($userId));
+}
+
+
+// TABLE_TO_IDENTIFYにユーザーidがあるか調べる すでにある場合true ない場合はfalseを返す
+function is_registeredUserId($userId){
+  $dbh = dbConnection::getConnection();
+  // pgp_sym_decryptは複合化
+  $sql = 'select userid from' . TABLE_TO_IDENTIFY . 'where ? =
+  (pgp_sym_decrypt(?,\'' . getenv('DB_ENCRYPT_PASS') . '\') )' ;
+  $sth = $dbh->prepare($sql);
+  $sth->execute(array($userId));
+  // レコードが存在しなければfalse
+  if(!($row = $sth->fetch())){
+    return false;
+  }else{
+    // ある場合はtrue
+    return true;
+  }
+}
+  
+  
+  
+
  ?>
